eadb60fdb40770696f5c0a98f20120c740fd92c8
pooyan <pooyansoltani@gmail.com>
2022-03-26 11:00:39 +0430

    Bubble_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,14 @@
-# Bubble Sort is the simplest sorting algorithm
-# that works by repeatedly swapping the adjacent elements if they are in wrong order.
-# The function always runs O(n^2) time even if the array is sorted
+# Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are
+# in wrong order. The function always runs O(n^2) time even if the array is sorted Bubble sort, sometimes referred to
+# as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements
+# and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted. The
+# algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to the top of the
+# list.
+
+# This simple algorithm performs poorly in real world use and is used primarily as an educational tool. More
+# efficient algorithms such as quicksort, timsort, or merge sort are used by the sorting libraries built into popular
+# programming languages such as Python and Java
+
 def bubble_sort(arr):
     length = len(arr)
 
